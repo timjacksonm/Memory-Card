1911d89d88ed8d0c29c3eaa0d7d8d2ad0d777f84
Tim Jackson <timjacksonm@gmail.com>
2021-05-04 00:42:27 -0500

    added a new div to return section of GameCards.js. The div acts as a screen that overlays the cards and has an onClick handler.

--- src/components/GameCards.js
+++ src/components/GameCards.js
@@ -2,47 +2,66 @@ import { useState, useEffect } from 'react';
 import characters from './Characters';
 
 function GameCards() {
-
+  const masterList = characters;
   const [charLimit, setCharLimit] = useState(12);
-  const [characterList, setCharacterList] = useState(characters)
 
-  function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-  function getRandomList() {
+  const getRandomList = () => {
     const randomIDs = [];
+
+    function getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
     while (randomIDs.length !== charLimit) {
       const chosenNum = Math.round(getRandomArbitrary(1, 55));
 
-      if (randomIDs.length < charLimit -1 && randomIDs.includes(chosenNum) === false) {
+      if (
+        randomIDs.length < charLimit - 1 &&
+        randomIDs.includes(chosenNum) === false
+      ) {
         randomIDs.push(chosenNum);
-      } else if (randomIDs.length < charLimit && randomIDs.includes(chosenNum) === false && characterList[chosenNum - 1].checked === 'false') {
+      } else if (
+        randomIDs.length < charLimit &&
+        randomIDs.includes(chosenNum) === false &&
+        masterList[chosenNum - 1].checked === false
+      ) {
         randomIDs.push(chosenNum);
       } else {
         //score 55 = win
       }
     }
-    return characterList.filter(character => randomIDs.includes(character.id))
-  }
-  // console.log(setCharacterList(characterList[0].checked = 'true'))
-  console.log(characterList)
-    return (
-        <div className='cards'>
-            {getRandomList().map(({ id, src, name }) => {
+    return masterList.filter((character) => randomIDs.includes(character.id));
+  };
+
+  const [characterList, setCharacterList] = useState(getRandomList());
+
+  const clickEvent = (e) => {
+    const num = e.target.id;
+    masterList[num - 1].checked = true;
+    setCharacterList(getRandomList());
+  };
+  return (
+    <div className='cards'>
+      {characterList.map(({ id, src, name }) => {
         return (
-          <div>
-            <img
-              key={id}
-              src={process.env.PUBLIC_URL + src}
-              alt={'image of ' + name}
-            />
-            <p>{name}</p>
+          <div key={id} className='individualCard'>
+            <span
+              className='screenOverCharacters'
+              onClick={clickEvent}
+              id={id}
+            ></span>
+            <div className='characterContainer'>
+              <input
+                src={process.env.PUBLIC_URL + src}
+                alt={'image of ' + name}
+                type='image'
+              />
+              <p>{name}</p>
+            </div>
           </div>
-        )
-              
+        );
       })}
-        </div>
-    )
+    </div>
+  );
 }
 export default GameCards;
