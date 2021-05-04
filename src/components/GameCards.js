2292f25a4404dcf9cf8f95f5eb3565654cb202b1
Tim Jackson <timjacksonm@gmail.com>
2021-05-03 23:02:16 -0500

    created function that creates a random scrambled list of characters to display to screen.

--- src/components/GameCards.js
+++ src/components/GameCards.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import characters from './Images';
+import { useState, useEffect } from 'react';
+import characters from './Characters';
 
 function GameCards() {
+
+  const [charLimit, setCharLimit] = useState(12);
+  const [characterList, setCharacterList] = useState(characters)
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function getRandomList() {
+    const randomIDs = [];
+    while (randomIDs.length !== charLimit) {
+      const chosenNum = Math.round(getRandomArbitrary(1, 55));
+
+      if (randomIDs.length < charLimit -1 && randomIDs.includes(chosenNum) === false) {
+        randomIDs.push(chosenNum);
+      } else if (randomIDs.length < charLimit && randomIDs.includes(chosenNum) === false && characterList[chosenNum - 1].checked === 'false') {
+        randomIDs.push(chosenNum);
+      } else {
+        //score 55 = win
+      }
+    }
+    return characterList.filter(character => randomIDs.includes(character.id))
+  }
+  // console.log(setCharacterList(characterList[0].checked = 'true'))
+  console.log(characterList)
     return (
         <div className='cards'>
-            {characters.map(({ id, src, name }) => {
+            {getRandomList().map(({ id, src, name }) => {
         return (
           <div>
             <img
@@ -14,7 +39,8 @@ function GameCards() {
             />
             <p>{name}</p>
           </div>
-        );
+        )
+              
       })}
         </div>
     )
